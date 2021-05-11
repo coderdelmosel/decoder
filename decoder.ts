d08434938e82d5cc32674f33fb5ae9a0da82e6e6
coderdelmosel <83160320+coderdelmosel@users.noreply.github.com>
2021-05-11 21:57:43 +0200

    convert

--- decoder.ts
+++ decoder.ts
@@ -7,4 +7,18 @@ function decoder(integers: number[], primes: [number, number, number]) {
   strictEqual(xPrime.toString().length, 4)
   strictEqual(yPrime.toString().length, 5)
   strictEqual(zPrime.toString().length, 3)
+  
+  const passwordAsNumber = Math.floor(primeConverter(...primes))
+  const fullPassword = BigInt(passwordAsNumber).toString()
+  const password = fullPassword.substring(0, integers.length)
+
+  const result = []
+  for (let i = 0; i < integers.length; i++) {
+    const integer = integers[i]
+    const passwordChar = password.charAt(i)
+    const res = integer ^ parseInt(passwordChar)
+    result.push(res)
+  }
+
+  console.log(result.join(''))
 }
