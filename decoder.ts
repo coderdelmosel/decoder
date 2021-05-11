3658c3446bff38a951b84836506a12ea45130694
coderdelmosel <83160320+coderdelmosel@users.noreply.github.com>
2021-05-11 21:57:11 +0200

    test inputs

--- decoder.ts
+++ decoder.ts
@@ -2,5 +2,9 @@ import { ok, strictEqual } from 'assert'
 import { primeConverter } from './primeConverter'
 
 function decoder(integers: number[], primes: [number, number, number]) {
-  
+  const [xPrime, yPrime, zPrime] = primes
+
+  strictEqual(xPrime.toString().length, 4)
+  strictEqual(yPrime.toString().length, 5)
+  strictEqual(zPrime.toString().length, 3)
 }
